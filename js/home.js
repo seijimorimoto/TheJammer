4fac597dab1f7ba056c98cd986b4700593c79b52
Angel Seiji Morimoto Burgos <angel_seiji@hotmail.com>
2018-09-14 19:42:27 -0500

    - Added AJAX to retrieve comments from an xml file. - Added username and complete name on comments displayed on home page.

--- js/home.js
+++ js/home.js
@@ -6,7 +6,7 @@ $postBox.css('height', this.scrollHeight);
 // addCommentBtn is enabled/disabled based on whether there is text on the postBox. 
 $postBox.on('input', function(event) {
   autoresize(this);
-  if ($(this).val() === "") {
+  if ($(this).val().trim() === "") {
     $('#addCommentBtn').prop('disabled', true);
   } else {
     $('#addCommentBtn').prop('disabled', false);
@@ -37,9 +37,51 @@ function autoresize(textarea) {
 // When the addCommentBtn is clicked, appends a comment to the end of the commentArea.
 $('#addCommentBtn').on('click', function(event) {
   let commentText = $('#postBox').val();
-  let newHtml = `<div class="commentBox twoColumnGrid">
-                  <img class="userImage" src="images/default_user_image.png" alt="User Image">
-                  <div class="comment">${commentText}</div>
-                 </div>`;
-  $('#commentArea').append(newHtml);
+  prependComment('Thomas Omaley', 'lab5', commentText); // Temporary credentials.
 });
+
+// AJAX GET request executed when the page is loaded. Retrieves comments stored in an xml and adds
+// it to the page.
+$.ajax({
+  url: './assets/comments.xml',
+  type: 'GET',
+  dataType: 'xml',
+  success: function(data) {
+    $(data).find('contact').each(function() {
+      let $name = $(this).find('name');
+      let commentText = $(this).find('text').text();
+      appendComment($($name).text(), $($name).attr('username'), commentText);
+    })
+  },
+  error: function(err) {
+    console.log(err);
+  }
+});
+
+// Adds a comment at the end of the river of comments in the home page.
+function appendComment(completeName, username, commentText) {
+  let newHtml = createCommentAsHtml(completeName, username, commentText);
+  $('#commentArea').append(newHtml);
+}
+
+// Adds a comment to the beginning of the river of comments in the home page.
+function prependComment(completeName, username, commentText) {
+  let newHtml = createCommentAsHtml(completeName, username, commentText);
+  $('#commentArea').prepend(newHtml);
+}
+
+// Creates HTML content to hold a comment and to display it appropriately on the home page.
+function createCommentAsHtml(completeName, username, commentText) {
+  return `<div class="commentBox twoColumnGrid">
+            <img class="userImage" src="images/default_user_image.png" alt="User Image">
+            <div class="comment">
+              <div class="commentHeader">
+                <span class="completeName">${completeName}</span>
+                <span class="username">@${username}</span>
+              </div>
+              <div class="commentBody">
+                ${commentText}
+              </div>
+            </div>
+          </div>`;
+}
